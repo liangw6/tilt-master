0bf922d03ffe41cb36cf67244aea778d3467aefb
Arthur <liangw6@cs.washington.edu>
2020-05-03 13:04:56 -0700

    Initial Commit

---
+++ cse562hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cse562hw2
+//
+//  Created by Liang Arthur on 5/3/20.
+//  Copyright © 2020 Liang Arthur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
