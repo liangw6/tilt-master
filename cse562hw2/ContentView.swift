7ac7f719c73ef83293ea83c39e8912190c18ca9e
Arthur <liangw6@cs.washington.edu>
2020-05-09 23:46:50 -0700

    makes it possible to open an output temporary file that can be shared through airdrop

--- cse562hw2/ContentView.swift
+++ cse562hw2/ContentView.swift
@@ -8,12 +8,74 @@
 
 import SwiftUI
 import CoreMotion
+import UIKit
+
+struct DocumentPreview: UIViewControllerRepresentable {
+    private var isActive: Binding<Bool>
+    private let viewController = UIViewController()
+    private let docController: UIDocumentInteractionController
+
+    init(_ isActive: Binding<Bool>, url: URL) {
+        self.isActive = isActive
+        self.docController = UIDocumentInteractionController(url: url)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPreview>) -> UIViewController {
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<DocumentPreview>) {
+        if self.isActive.wrappedValue && docController.delegate == nil { // to not show twice
+            docController.delegate = context.coordinator
+            self.docController.presentPreview(animated: true)
+        }
+    }
+
+    func makeCoordinator() -> Coordintor {
+        return Coordintor(owner: self)
+    }
+
+    final class Coordintor: NSObject, UIDocumentInteractionControllerDelegate { // works as delegate
+        let owner: DocumentPreview
+        init(owner: DocumentPreview) {
+            self.owner = owner
+        }
+        func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+            return owner.viewController
+        }
+
+        func documentInteractionControllerDidEndPreview(_ controller: UIDocumentInteractionController) {
+            controller.delegate = nil // done, so unlink self
+            owner.isActive.wrappedValue = false // notify external about done
+        }
+    }
+}
 
 struct ContentView: View {
     let data_names = ["x", "y", "z"]
+    let temp_file_name = "temp_tile_store.txt"
+    let url = getDocumentsDirectory().appendingPathComponent("output.txt")
+    
     @ObservedObject var motionData = MotionDataProcessor()
+    @State private var show_doc = false
     var body: some View {
         VStack {
+            Button(action: {
+                let str = "Super long string here"
+                
+                do {
+                    try str.write(to: self.url, atomically: true, encoding: String.Encoding.utf8)
+                    print("wrote to file at url \(self.url)")
+                } catch {
+                    // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+                    print("write to file errored!!!")
+                }
+                
+                self.show_doc = true
+                
+            }) {
+                Text("Write File")
+            }.background(DocumentPreview($show_doc, url: url))
             Button(action: {
                     print("button pressed")
                     self.motionData.start()
@@ -61,7 +123,10 @@ struct ContentView: View {
     }
 }
 
-
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
